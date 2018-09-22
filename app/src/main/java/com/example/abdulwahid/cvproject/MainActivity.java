33569cdbf2116a6fe72add23788b41db278b19db
Abdul Wahid <bsef15a018@pucit.edu.pk>
2018-09-22 11:11:42 +0500

    i have reverted the changes

--- app/src/main/java/com/example/abdulwahid/cvproject/MainActivity.java
+++ app/src/main/java/com/example/abdulwahid/cvproject/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.abdulwahid.cvproject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -12,6 +16,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.isityou.sdk.IsItYouSdk;
@@ -19,17 +24,27 @@ import com.isityou.sdk.IsItYouSdk;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG ="MainActivity" ;
     int REQUEST_CODE = 0;
     private IsItYouSdk iiy;
     private Bitmap myBitmap;
     public static int count = 0;
     private static final int CAMERA_ROTATION = 0 ;
     private static final String MY_CODE = "R3JoeBpA271NzmQ8";
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,32 +55,111 @@ public class MainActivity extends AppCompatActivity {
         iiy.setAS(false);
 
         int initResult = iiy.init(MY_CODE, CAMERA_ROTATION);
-        Toast.makeText(this, ""+initResult, Toast.LENGTH_SHORT).show();
+        //  Toast.makeText(this, ""+initResult, Toast.LENGTH_SHORT).show();
 
         String initUserResult = iiy.initUser("Guest");
-        Toast.makeText(this, initUserResult, Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, initUserResult, Toast.LENGTH_LONG).show();
 
-        Button capture = (Button) findViewById(R.id.btnCapture);
-        capture.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
 
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, REQUEST_CODE);
-            }
-        });
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
+        Button captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        mCamera.takePicture(null, null, mPicture);
+                    }
+                }
+        );
     }
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            myBitmap=photo;
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
 
-            int response=iiy.saveEnrollment(getBytesFromBitmap(myBitmap),0);
-            Toast.makeText(this, ""+response, Toast.LENGTH_SHORT).show();
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            Toast.makeText(MainActivity.this, "picture taken", Toast.LENGTH_SHORT).show();
+            int num=iiy.saveEnrollment(data,0);
+
+            Toast.makeText(MainActivity.this, ""+num, Toast.LENGTH_SHORT).show();
+            /*
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions");
+                return;
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+            */
+        }
+    };
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(1); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
         }
     }
-    public byte[] getBytesFromBitmap(Bitmap bitmap) {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
-        return stream.toByteArray();
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
     }
+
+
 }
